16cc92fbd3b3441f21e82339d9971a4215630eb3
Matilde <matildemcr@ua.pt>
2024-11-01 11:22:01 +0000

    function remove_deleted_files

--- backup_files.sh
+++ backup_files.sh
@@ -88,6 +88,20 @@ copy_file() {
 }
 
 
+# funtcion to remove the files in the backup that are not in SRC
+remove_deleted_files() {
+    for file in "${files_in_dst[@]}"; do
+        src_file="${SRC}/$(basename "$file")"
+
+        if ! [[ -f "${src_file}" ]]; then
+            deleted=$(($deleted + 1))
+            size_deleted=$(($size_deleted + $(stat -c%s "$file")))
+        fi 
+    done
+}
+
+
+
 #if c was passed in as a flag then simulate the backup
 if (( ${checking} == 1 )); then
     #simulate the backup
